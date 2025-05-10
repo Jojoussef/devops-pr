6d6928a394e13ed97f5932b0098b05a748677855
ramiibrahim2002 <ramybrahim77@gmail.com>
2025-05-10 17:50:17 +0100

    endpoint model view serializer

--- back/application_layer/app/views.py
+++ back/application_layer/app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import ToDoItem
+from .serializers import TodoItemSerializer
 
-# Create your views here.
+class TodoItemViewSet(viewsets.ModelViewSet):
+    """
+    list, create, retrieve, update, destroy all provided by DRF
+    """
+    queryset = ToDoItem.objects.all().order_by('-created_at')
+    serializer_class = TodoItemSerializer
+
+    @action(detail=True, methods=['post'])
+    def mark_complete(self, request, pk=None):
+        """
+        POST /api/todos/{pk}/mark_complete/
+        """
+        todo = self.get_object()
+        todo.completed = True
+        todo.save()
+        return Response(self.get_serializer(todo).data)
